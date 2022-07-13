5a8f952dd017a9884a3df26e2830ffb18600c4a1
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-07-13 18:52:34 +0100

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="btn" :style="{ background: color }" @click="onClick()">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+        console.log('click')
+    }
+  }
+};
+</script>
+
+<style>
+</style>
